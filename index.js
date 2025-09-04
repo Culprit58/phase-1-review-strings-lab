94335c41e5e9a7d92ec63595df47bb613618c3a4
Enock Cheruiyot <cheruiyote518@gmail.com>
2025-09-04 19:43:16 +0300

    lab down

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "enock";
+const welcomeMessage = "Welcome to Flatbook, enock!";
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
